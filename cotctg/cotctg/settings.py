43aae5edc28dac1ac6291ec17863a0f98050e209
Hugo Chavero <hugochavero@gmail.com>
2017-10-17 00:53:49 -0300

    Actualiza ALLOWED_HOST

--- cotctg/cotctg/settings.py
+++ cotctg/cotctg/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8i4c=su!t-v+2c)jbrk)qo=)+=#amhkw8j(k&h9-n!=qvulm(%'
 
+ALLOWED_HOSTS = ['104.131.70.120']
 
 SITE_ID = 1
 
